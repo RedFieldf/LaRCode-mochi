5b7aa2da760c0cd19cf45fa2348316f066e3d1e5
kkito0726 <k.kito0726@gmail.com>
2023-11-29 17:01:34 +0900

    B問題回答完了

--- ABC_246_B.py
+++ ABC_246_B.py
@@ -1,6 +1,14 @@
-'''
+"""
 AtCoder Beginner Contest 246
 B問題
 URL: https://atcoder.jp/contests/abc246/tasks/abc246_b
-'''
+"""
+import math
 
+A, B = map(int, input().split())
+
+# ベクトルの大きさを計算
+length = math.sqrt((A**2 + B**2))
+
+# 位置ベクトルを計算して出力
+print(A / length, B / length)
