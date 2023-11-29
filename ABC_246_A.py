bcd8e17c22faa59772cd5cff8ffe0d16ce1239d8
kkito0726 <k.kito0726@gmail.com>
2023-11-29 17:59:00 +0900

    A問題回答完了

--- ABC_246_A.py
+++ ABC_246_A.py
@@ -1,5 +1,19 @@
-'''
+"""
 AtCoder Beginner Contest 246
 A問題
 URL：https://atcoder.jp/contests/abc246/tasks/abc246_a
-'''
+"""
+nums = [list(map(int, input().split())) for _ in range(3)]
+
+# x座標だけのリストとy座標だけのリストに分ける
+x = [num[0] for num in nums]
+y = [num[1] for num in nums]
+
+# 各座標でユニークな数値が答えの座標になる
+ans = [0, 0]
+for i in range(3):
+    if x.count(x[i]) == 1:
+        ans[0] = x[i]
+    if y.count(y[i]) == 1:
+        ans[1] = y[i]
+print(*ans)
